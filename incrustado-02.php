3b5d895751fac814670b5287e3747931fb1781f6
larcery <75611800@idexperujapon.edu.pe>
2024-08-27 18:02:36 -0500

    agree incrustado 01 y 02

---
+++ incrustado-02.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>incrustado 02</title>
+</head>
+<body>
+    <p> 
+        <?php
+        echo "Larcery Lopez Bazan ";
+        ?>
+    </p>
+    
+</body>
+</html>
